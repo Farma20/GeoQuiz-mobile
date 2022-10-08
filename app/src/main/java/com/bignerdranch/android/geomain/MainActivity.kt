450bbf1a8659ba06684ddc44f60ee37e55837289
ÐFarma20 <kustov.ius@students.dvfu.ru>
2022-10-08 13:07:16 +1000

    Реализована возможность смены вопросов, реализовано отслеживание правильных и неправильных ответов на вопросы

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,9 +2,9 @@ package com.bignerdranch.android.geomain
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,36 +13,72 @@ class MainActivity : AppCompatActivity() {
     // lateinit обещает JVM, что мы инициализируем эту переменную
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    //Создаем неизменяемый список с вопросами и ответами
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Достаем кнопки по их id, описанному в xml
+        // Достаем виджеты по их id, описанному в xml
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
 
         //Добавление слушателей на кнопки
         trueButton.setOnClickListener{view: View ->
-
             //Создаем всплывающее окно с текстом (Toast) при нажатии
-
-            var toast: Toast = Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT
-            )
-            toast.show()
+            checkAnswer(true)
 
         }
 
-        falseButton.setOnClickListener{view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-            ).show()
-
+        falseButton.setOnClickListener{ view: View ->
+            checkAnswer(false)
         }
+
+        nextButton.setOnClickListener { view: View ->
+            //Изменяеи idx при нажатии на next
+            currentIndex = (currentIndex + 1) % questionBank.size
+
+            updateQuestion()
+        }
+
+       updateQuestion()
+    }
+
+    //Достаем idx актуального вопроса и вставляем его в виджет
+    private fun updateQuestion(){
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    //Функция проверки вопроса
+    private fun checkAnswer(userAnswer: Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if(userAnswer == correctAnswer){
+            R.string.correct_toast
+        } else{
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(
+            this,
+            messageResId,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
